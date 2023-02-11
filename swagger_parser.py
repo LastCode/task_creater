7ec4b4139bcd783f65b6e7282264719fd8a385f1
Francis <forgot1972@gmail.com>
2023-02-11 06:39:56 -0500

    Update swagger_parser.py

--- swagger_parser.py
+++ swagger_parser.py
@@ -1,22 +1,22 @@
 import json
+import openapi_spec_parser
 
+def parser():
 
-def get_payload_parameters(file_path=None):
-    payload_parameters={}
-    with open(file_path) as f:
-        swagger_data = json.load(f)
-    
-    # Get the payload parameters information from the Swagger data
-    parameters = swagger_data["paths"]["/example"]["post"]["parameters"]
-    
-    # Iterate over the parameters and print each one
-    for parameter in parameters:
-        print("Name:", parameter["name"])
-        print("In:", parameter["in"])
-        print("Required:", parameter.get("required", False))
-        print("Type:", parameter["schema"]["type"])
-        print("\n")
-        
-if __name__ == '__main__':
-    get_payload_parameters('')
+    # Load the Swagger JSON file
+    with open("swagger.json", "r") as file:
+        swagger_json = json.load(file)
+
+    # Parse the Swagger JSON file
+    spec = openapi_spec_parser.load_spec_from_dict(swagger_json)
+
+    # Extract the request payload for a specific endpoint
+    endpoint = "/example/{id}"
+    method = "post"
+    operation = spec.get_path(endpoint).operations[method]
+    payload = operation.request_body.to_dict()
+
+    # Print the extracted payload
+    print(payload)
+
 
